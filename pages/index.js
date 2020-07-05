1bf3ac0cd6124677d80221d5921c33136e2d0b8b
rasid bram <rashidaynur.ibram@yandex.com>
2020-07-05 18:47:11 +0100

    index was done more readable

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,9 @@
 import React from 'react'
+
 import Layout from '../components/layout'
-import useWindowSize from '../hooks/useWindowSize'
-import CONST from '../constants/index'
-import Sidebar from '../components/col-sidebar'
-import Main from '../components/col-main'
-import Extra from '../components/col-extra'
 
 function HomePage() {
-    const size = useWindowSize()
-    return (
-        <div>
-            <Layout>
-                <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
-                <Main>{JSON.stringify(size)}</Main>
-                {size.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
-            </Layout>
-        </div>
-    )
+  return <Layout>Anasayfa</Layout>
 }
 
 export default HomePage
